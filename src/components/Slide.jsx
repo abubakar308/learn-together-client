0fb20880095527eabdee22c887bb069375a67c11
abubakar308 <mdabubakarsiddique789@gmail.com>
2024-12-24 12:51:51 +0600

    stats and other updated

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -2,11 +2,11 @@
 
 const Slide = ({image, text}) => {
     return (
-        <div className="w-full bg-center rounded-2xl bg-cover h-[38rem]"
+        <div className="md:w-full bg-center rounded-2xl bg-cover h-[20rem] md:h-[36rem]"
         style={{
             backgroundImage: `url(${image})`
         }}>
-            <p className="text-5xl font-black text-center py-5">
+            <p className=" text-2xl md:text-5xl font-black text-center py-5">
                 {text}
             </p>
            
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,8 +2,23 @@
 
 const Stats = () => {
     return (
-        <div>
-            stats
+        <div className="grid md:grid-cols-4">
+            <div>
+                <h2 className="text-5xl font-bold">0</h2>
+                <p>Experienced tutors</p>
+            </div>
+            <div>
+            <h2 className="text-5xl font-bold">0</h2>
+            <p>Review</p>
+            </div>
+            <div>
+            <h2 className="text-5xl font-bold">0</h2>
+            <p>Total Languages</p>
+            </div>
+            <div>
+            <h2 className="text-5xl font-bold">0</h2>
+            <p>Total users</p>
+            </div>
         </div>
     );
 };

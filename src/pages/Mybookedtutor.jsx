3b20457d4bf4cc0b03467c98e3fd50c213775331
abubakar308 <mdabubakarsiddique789@gmail.com>
2025-06-28 23:02:38 +0600

    mybookedtutor page updated

--- src/pages/Mybookedtutor.jsx
+++ src/pages/Mybookedtutor.jsx
@@ -18,7 +18,6 @@ const Mybookedtutor = () => {
         },[user.email]);
 
         const handleReview = (tutorId) =>{
-          console.log(tutorId)
           fetch(`${import.meta.env.VITE_API_URL}/bookedtutor/${ tutorId}`,{
             method: "PATCH",
             headers: {
@@ -32,15 +31,15 @@ const Mybookedtutor = () => {
           .then(data=>console.log(data))
         }
     return (
-      <div className="p-6 w-11/12 mx-auto mt-20 bg-background">
-      <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">
+      <div className="p-6 w-11/12 mx-auto mt-20">
+      <h1 className="text-3xl font-bold text-center mb-6 text-primary">
         Booked Tutors
       </h1>
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {tutors.map((tutor) => (
           <div
             key={tutor?._id}
-            className="bg-white shadow-md rounded-lg overflow-hidden"
+            className="bg-background dark:bg-darkBackground shadow-lg rounded-xl overflow-hidden hover:shadow-xl transition"
           >
             <img
               src={tutor?.image}
@@ -49,32 +48,27 @@ const Mybookedtutor = () => {
             />
             <div className="p-4">
               {/* Tutor's Name */}
-              <h2 className="text-xl font-bold text-gray-800">
+              <h2 className="text-xl font-bold text-gray-800 dark:text-white">
                 {tutor.displayName}
               </h2>
     
               {/* Language */}
-              <p className="text-gray-600 mt-2">
+              <p className="text-gray-600 dark:text-gray-300 mt-2">
                 <span className="font-medium text-primary">Language:</span>{" "}
                 {tutor?.language}
               </p>
     
               {/* Price */}
-              <p className="text-gray-600 mt-2">
+              <p className="text-gray-600 dark:text-gray-300 mt-2">
                 <span className="font-medium text-primary">Price:</span> BDT{" "}
                 {tutor?.price}
               </p>
     
-              {/* Review */}
-              <p className="text-gray-600 mt-2">
-                <span className="font-medium text-primary">Review Count:</span>{" "}
-                {tutor?.review}
-              </p>
     
               {/* Review Button */}
               <button
                 onClick={() => handleReview(tutor.tutorId)}
-                className="mt-4 w-full bg-primary text-white py-2 px-4 rounded-md font-medium hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-primary"
+                 className="mt-4 w-full bg-secondary hover:bg-amber-600 text-white py-2 px-4 rounded-lg font-semibold transition"
               >
                 Leave Review
               </button>
